f60e10c659e3fa72ed1ab3bc67f1708ab016f024
Selin Tuerkoglu <selin.t@live.de>
2025-06-08 01:20:32 +0200

    Initial Commit

---
+++ TransparentWidgetsDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TransparentWidgetsDemo
+//
+//  Created by Selin Türkoglu on 08.06.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TransparentWidgetsDemo/TransparentWidgetsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransparentWidgetsDemoApp.swift
+//  TransparentWidgetsDemo
+//
+//  Created by Selin Türkoglu on 08.06.25.
+//
+
+import SwiftUI
+
+@main
+struct TransparentWidgetsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
